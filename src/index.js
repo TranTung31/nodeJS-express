8b05005fedfde21e4410b9d14f7465900af7e33e
Your Name <you@example.com>
2023-10-05 09:53:14 +0700

    9 - POST method

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Templace engine
 app.engine(
@@ -31,6 +38,11 @@ app.get("/news", (req, res) => {
 
 app.get("/search", (req, res) => {
   res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.send("");
 });
 
 app.listen(port, () => {
